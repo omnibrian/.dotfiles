c94de667430aa148342eb4d2c445ff4c382f19ea
Brian Meneley <brianmeneley@gmail.com>
2022-02-18 15:01:36 -0400

    Fix polybar launch script and add emacs dots

--- bin/polybar/launch.sh
+++ bin/polybar/launch.sh
@@ -2,12 +2,10 @@
 #
 # Entrypoint for WM binding to start polybar
 
-if [[ -z "$(pgrep -u $UID -x polybar)" ]] ; then
-  # start polybar for all known monitor connections
-  for monitor in $(xrandr -q | grep connected | cut -d' ' -f1) ; do
-    MONITOR=$monitor polybar --quiet --reload main &
-  done
-else
-  # send restart command to existing polybar instances
-  polybar-msg cmd restart
-fi
+# cleanup running polybar instances
+killall -q polybar
+
+# start polybar for all known monitor connections
+for monitor in $(xrandr -q | grep connected | cut -d' ' -f1) ; do
+  MONITOR=$monitor polybar --quiet --reload main &
+done
