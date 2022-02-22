d4cdbca5df2c55a60f3faa1dd01693573367d0bf
Brian Meneley <brianmeneley@gmail.com>
2022-02-22 09:31:44 -0400

    Add libinput-gestures config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,6 +36,10 @@
 
 ;; ================ colors =============================================
 (load-theme 'brian t)
+
+;; highlight current line
+(global-hl-line-mode)
+(set-face-background hl-line-face "#272931")  ; dark-dark-gray
 ;; ================ colors =============================================
 
 
@@ -70,3 +74,33 @@
 ;; ================ whitespace =========================================
 (add-hook 'write-file-hooks 'delete-trailing-whitespace)
 ;; ================ whitespace =========================================
+
+
+;; ================ packages ===========================================
+(defvar bootstrap-version)
+
+(let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; helm -- github.com/emacs-helm/helm
+(straight-use-package 'helm)
+
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+;; ================ packages ===========================================
+
+
+;; ================ misc ===============================================
+;; always follow symlinks to version controlled files
+(setq vc-follow-symlinks t)
+;; ================ misc ===============================================
