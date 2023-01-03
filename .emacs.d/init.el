235e69546a927da0b4447b704ae8dbcb836347b2
Brian Meneley <brianmeneley@gmail.com>
2023-01-03 11:10:18 -0400

    Update tmux to emacs keys and add emacs windmoves

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -152,6 +152,11 @@
 (global-set-key (kbd "C-c j") 'windmove-down)
 (global-set-key (kbd "C-c k") 'windmove-up)
 (global-set-key (kbd "C-c l") 'windmove-right)
+
+(global-set-key (kbd "C-c C-h") 'windmove-left)
+(global-set-key (kbd "C-c C-j") 'windmove-down)
+(global-set-key (kbd "C-c C-k") 'windmove-up)
+(global-set-key (kbd "C-c C-l") 'windmove-right)
 ;; ================ window switching ===================================
 
 
